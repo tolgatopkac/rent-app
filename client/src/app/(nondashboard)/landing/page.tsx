c7602f332f7f8995d8807d18a8b6e75f14717be4
tolgatopkac-da <72669663+tolgatopkac@users.noreply.github.com>
2025-07-10 11:35:44 +0300

    add features section

--- client/src/app/(nondashboard)/landing/page.tsx
+++ client/src/app/(nondashboard)/landing/page.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import { HeroSection } from "./HeroSection";
+import FeaturesSection from "./FeaturesSection";
 
 function Landing() {
-  return <HeroSection />;
+  return (
+    <div>
+      <HeroSection />
+      <FeaturesSection />
+    </div>
+  );
 }
 
 export default Landing;
