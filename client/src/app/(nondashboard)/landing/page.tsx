8d0482a92184590f37c1cbefa3188af6d8527410
tolgatopkac-da <72669663+tolgatopkac@users.noreply.github.com>
2025-07-10 14:21:37 +0300

    add cta and discover section

--- client/src/app/(nondashboard)/landing/page.tsx
+++ client/src/app/(nondashboard)/landing/page.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import { HeroSection } from "./HeroSection";
 import FeaturesSection from "./FeaturesSection";
+import DiscoverSection from "./DiscoverSection";
+import CallToActionSection from "./CallToActionSection";
 
 function Landing() {
   return (
     <div>
       <HeroSection />
       <FeaturesSection />
+      <DiscoverSection />
+      <CallToActionSection />
     </div>
   );
 }
