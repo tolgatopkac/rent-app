908f59e5e032050e621c428d39768f03b3f25844
tolgatopkac-da <72669663+tolgatopkac@users.noreply.github.com>
2025-07-10 21:03:01 +0300

    setup backend file and script

---
+++ server/src/index.ts
@@ -0,0 +1,29 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import bodyParser from "body-parser";
+
+/* route import */
+
+/* configurations */
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+/* ROUTES */
+app.get("/", (req, res) => {
+  res.send("This is home route");
+});
+
+/* Server */
+const port = process.env.PORT || 3002;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
